b103812d70468c2b31059dafc9baa9bee3374fcb
Navin Pai <lifeofnavin@gmail.com>
2013-09-19 13:25:49 +0530

    Added Maven Project Template

---
+++ src/main/java/org/iiitb/os/os_proj/App.java
@@ -0,0 +1,13 @@
+package org.iiitb.os.os_proj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
