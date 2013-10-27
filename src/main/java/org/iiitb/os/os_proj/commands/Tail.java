0aaa754c9498c7dfce33d334b2ac5a7ce1622d00
Navin Pai <lifeofnavin@gmail.com>
2013-10-28 03:38:33 +0530

    Fixed typos

--- src/main/java/org/iiitb/os/os_proj/commands/Tail.java
+++ src/main/java/org/iiitb/os/os_proj/commands/Tail.java
@@ -7,7 +7,7 @@ public class Tail implements ICommand {
 
 	public ArrayList<String> runCommand(ArrayList<String> params) {
 		ArrayList<String> result=new ArrayList<String>();
-		//same as cat, but show only last 100 lines. If file is smaller, just show entire file
+		//same as cat, but show only last 100 lines.If file is smaller, just show entire file
 		return result;	
 	}
 
