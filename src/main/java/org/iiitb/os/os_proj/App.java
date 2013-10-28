b9226b1e7e53e7a8aae121ab2593e359c8d895e5
KanchanGupta <kanchan@kanchan-HP-G42-Notebook-PC>
2013-10-28 23:16:03 +0530

    locate implemented

--- src/main/java/org/iiitb/os/os_proj/App.java
+++ src/main/java/org/iiitb/os/os_proj/App.java
@@ -1,5 +1,8 @@
 package org.iiitb.os.os_proj;
 
+import java.util.ArrayList;
+
+import org.iiitb.os.os_proj.commands.Locate;
 import org.iiitb.os.os_proj.shell.Shell;
 
 /**
@@ -14,5 +17,11 @@ public class App
         shell.setSize(500,500);
 		shell.setDefaultCloseOperation(javax.swing.JFrame.DISPOSE_ON_CLOSE);
 		shell.setVisible(true);
+		Locate l=new Locate();
+		ArrayList<String> al=new ArrayList();
+		al.add("Locate");
+		al.add("kanchu17");
+		l.runCommand(al);
+		
     }
 }
--- src/main/java/org/iiitb/os/os_proj/commands/Locate.java
+++ src/main/java/org/iiitb/os/os_proj/commands/Locate.java
@@ -1,13 +1,33 @@
 package org.iiitb.os.os_proj.commands;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.iiitb.os.os_proj.UserFile;
+import org.iiitb.os.os_proj.db.MongoConnectivity;
 
 public class Locate implements ICommand {
 
 	public ArrayList<String> runCommand(ArrayList<String> params) {
 		ArrayList<String> result=new ArrayList<String>();
+		int i = 0;
 		//Search db by name
-		//Add filepaths of results to result and return
+		MongoConnectivity testMongo=new MongoConnectivity();
+		Map<String, String> constraints = new HashMap<String, String>();
+		constraints.put("name", params.get(1));
+		ArrayList<UserFile> files = testMongo.getFiles(constraints);
+		if(files.size()>0){
+		while(i<files.size())
+		{
+		result.add(files.get(i).getPath());
+		}
+		}
+		else{
+		result.add("failure");
+		result.add("file does not located");
+		}
+		System.out.println(result);
 		return result;
 	}
 
