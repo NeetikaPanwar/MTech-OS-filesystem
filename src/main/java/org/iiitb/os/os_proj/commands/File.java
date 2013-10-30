ce7ad43f33adb39ce2802af1eb34b37136465d34
ankitsharma051089 <ankitsharma051089@gmail.com>
2013-10-30 19:30:05 +0530

    !!

--- src/main/java/org/iiitb/os/os_proj/commands/File.java
+++ src/main/java/org/iiitb/os/os_proj/commands/File.java
@@ -1,15 +1,40 @@
 package org.iiitb.os.os_proj.commands;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.iiitb.os.os_proj.UserFile;
+import org.iiitb.os.os_proj.db.MongoConnectivity;
 
 public class File implements ICommand {
 
 	public ArrayList<String> runCommand(ArrayList<String> params) {
+		int i = 0;
+		int receivedFileTypeId;
 		ArrayList<String> result=new ArrayList<String>();
+		ArrayList<UserFile> receivedFile = new ArrayList<UserFile>();
+		MongoConnectivity fileSearch = new MongoConnectivity();
+		Map<String, String> constraints = new HashMap<String, String>();
+		constraints.put("name", params.get(0));
+		receivedFile = fileSearch.getFiles(constraints);
+		
+		while(i<receivedFile.size())
+		{
+			receivedFileTypeId = receivedFile.get(i).getFiletypeId();
+			i++;
+		}
+		if(receivedFile.size()==0)
+		{
+			result.add("failure");
+			result.add("file does not exist");
+			
+		}
+		return result;
 		//search in db for file
 		//if exists, return success and filetype
 		//else return failure
-		return result;
+		
 	}
 
 }
--- src/main/java/org/iiitb/os/os_proj/commands/Filesize.java
+++ src/main/java/org/iiitb/os/os_proj/commands/Filesize.java
@@ -1,11 +1,35 @@
 package org.iiitb.os.os_proj.commands;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.iiitb.os.os_proj.UserFile;
+import org.iiitb.os.os_proj.db.MongoConnectivity;
 
 public class Filesize implements ICommand {
 
 	public ArrayList<String> runCommand(ArrayList<String> params) {
+		int i = 0;
+		int receivedFileSize;
 		ArrayList<String> result=new ArrayList<String>();
+		ArrayList<UserFile> receivedFile = new ArrayList<UserFile>();
+		MongoConnectivity fileSearch = new MongoConnectivity();
+		Map<String, String> constraints = new HashMap<String, String>();
+		constraints.put("name", params.get(0));
+		receivedFile = fileSearch.getFiles(constraints);
+		
+		while(i<receivedFile.size())
+		{
+			receivedFileSize = receivedFile.get(i).getFile_size();
+			i++;
+		}
+		if(receivedFile.size()==0)
+		{
+			result.add("failure");
+			result.add("file does not exist");
+			
+		}
 		//search in db for file
 		//if exists, return success and filesize
 		//else return failure
