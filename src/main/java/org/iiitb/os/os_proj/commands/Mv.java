c751f28999be206b0936492ae44dc091c8c69446
Navin Pai <lifeofnavin@gmail.com>
2013-11-16 02:58:01 +0530

    Fixed mv

--- src/main/java/org/iiitb/os/os_proj/commands/Mv.java
+++ src/main/java/org/iiitb/os/os_proj/commands/Mv.java
@@ -12,75 +12,109 @@ import com.mongodb.WriteResult;
 
 public class Mv implements ICommand {
 
-	public ArrayList<String> runCommand(List<String> params) {
-		ArrayList<String> result = new ArrayList<String>();
+    public ArrayList<String> runCommand(List<String> params) {
+        ArrayList<String> result = new ArrayList<String>();
 
-		// get src and dest
-		ArrayList<String> srcPath = GetPath.getSearchPath(params.get(0));
-		Map<String, Object> srcConstraints = new HashMap<String, Object>();
-		srcConstraints.put("name", srcPath.get(0));
-		srcConstraints.put("path", srcPath.get(1));
-		ArrayList<UserFile> srcFiles = mongoConnect.getFiles(srcConstraints);
+        // get src and dest
+        ArrayList<String> srcPath = GetPath.getSearchPath(params.get(0));
+        Map<String, Object> srcConstraints = new HashMap<String, Object>();
+        srcConstraints.put("name", srcPath.get(0));
+        srcConstraints.put("path", srcPath.get(1));
+        ArrayList<UserFile> srcFiles = mongoConnect.getFiles(srcConstraints);
 
-		ArrayList<String> destPath = GetPath.getSearchPath(params.get(1));
-		Map<String, Object> destConstraints = new HashMap<String,Object>();
-		destConstraints.put("name", destPath.get(0));
-		destConstraints.put("path", destPath.get(1));
-		ArrayList<UserFile> destFiles = mongoConnect.getFiles(destConstraints);
+        ArrayList<String> destPath = GetPath.getSearchPath(params.get(1));
+        Map<String, Object> destConstraints = new HashMap<String,Object>();
+        destConstraints.put("name", destPath.get(0));
+        destConstraints.put("path", destPath.get(1));
+        ArrayList<UserFile> destFiles = mongoConnect.getFiles(destConstraints);
 
-		if (srcFiles.size() == 0) {
-			result.add(ICommand.FAILURE);
-			result.add("mv: cannot stat '" + srcPath.get(0)
-					+ "': No such file or directory");
-		} 
-		else if (srcFiles.get(0).isDirectory() == false) // src is file
-		{
-			// move srcfile to destfile - rename filename and change path as
-			// dest path
-			if (destFiles.size() == 0) {
-				srcFiles.get(0).setName(destPath.get(0));
-				srcFiles.get(0).setPath(destPath.get(1));
-			} 
-			else if (!destFiles.get(0).isDirectory()) // move file to
-				// file(delete also)
-			{
-				mongoConnect.deleteFile(destPath.get(0), destPath.get(1));
-				srcFiles.get(0).setName(destPath.get(0));
-				srcFiles.get(0).setPath(destPath.get(1));
-			} 
-			else // move file to folder(update path of src)
-			{
-				srcFiles.get(0).setPath(destPath.get(1) + "/" + destPath.get(0));
-			}
+        if (srcFiles.size() == 0) {
+            result.add(ICommand.FAILURE);
+            result.add("mv: cannot stat '" + srcPath.get(0)
+                    + "': No such file or directory");
+        }
+        else if (!srcFiles.get(0).isDirectory()) // src is file
+        {
+            // move srcfile to destfile - rename filename and change path as
+            // dest path
+            if (destFiles.size() == 0) {
+                srcFiles.get(0).setName(destPath.get(0));
+                srcFiles.get(0).setPath(destPath.get(1));
+            }
+            else if (!destFiles.get(0).isDirectory()) // move file to
+            // file(delete also)
+            {
+                mongoConnect.deleteFile(destPath.get(0), destPath.get(1));
+                srcFiles.get(0).setName(destPath.get(0));
+                srcFiles.get(0).setPath(destPath.get(1));
+            }
+            else // move file to folder(update path of src)
+            {
+                srcFiles.get(0).setPath(destPath.get(1) + destPath.get(0) + "/");
+            }
 
-			WriteResult wr = mongoConnect.updateCommon(destFiles.get(0));
-			if (wr.getError() == null)
-				result.add(ICommand.SUCCESS);
-			else
-				result.add(ICommand.FAILURE);
+            WriteResult wr = mongoConnect.updateCommon(srcFiles.get(0));
+            if (wr.getError() == null)
+                result.add(ICommand.SUCCESS);
+            else
+                result.add(ICommand.FAILURE);
 
-		} 
-		else // src is a folder
-		{
-			// change the path of the src folder and all its contents
-			if (destFiles.size() == 0) // dest folder does not exist
-			{
+        }
+        else // src is a folder
+        {
+            // change the path of the src folder and all its contents
+            if (destFiles.size() == 0) // dest folder does not exist
+            {
+                Map<String, Object> pathConstraint = new HashMap<String, Object>();
+                String pathPattern = srcPath.get(1) + srcPath.get(0);
+                pathConstraint.put("path", java.util.regex.Pattern.compile(pathPattern));
+                ArrayList<UserFile> receivedFiles = mongoConnect.getFiles(pathConstraint);
+                //WriteResult wr;
+                for(UserFile u: receivedFiles)
+                {
+                    String path = destPath.get(1) + destPath.get(0);
+                    System.out.println(path.length()+ " "+path+ " "+pathPattern +" "+u.getPath());
+                    u.setPath(u.getPath().replace(pathPattern, path));
+                    //System.out.println(u.getPath());
+                    mongoConnect.updateCommon(u);
+                }
+                srcFiles.get(0).setName(destPath.get(0));
+                srcFiles.get(0).setPath(destPath.get(1));
+                mongoConnect.updateCommon(srcFiles.get(0));
+                //mongoConnect.deleteFile(destPath.get(0), destPath.get(1));
+                result.add(ICommand.SUCCESS);
 
-			} 
-			else if (destFiles.get(0).isDirectory()) // dest folder exist
-			{
+            }
+            else if (destFiles.get(0).isDirectory()) // dest folder exist
+            {
+                Map<String, Object> pathConstraint = new HashMap<String, Object>();
+                String pathPattern = srcPath.get(1) + srcPath.get(0);
+                pathConstraint.put("path", java.util.regex.Pattern.compile(pathPattern));
+                ArrayList<UserFile> receivedFiles = mongoConnect.getFiles(pathConstraint);
+                //WriteResult wr;
+                for(UserFile u: receivedFiles)
+                {
+                    String path = destPath.get(1) + destPath.get(0);
+                    u.getPath().replace(pathPattern, path);
+                    mongoConnect.updateCommon(u);
+                }
+                srcFiles.get(0).setName(destPath.get(0));
+                srcFiles.get(0).setPath(destPath.get(1));
+                mongoConnect.updateCommon(srcFiles.get(0));
+                mongoConnect.deleteFile(destPath.get(0), destPath.get(1));
+                result.add(ICommand.SUCCESS);
 
-			} 
-			else // move folder to file - give error
-			{
-				result.add(ICommand.FAILURE);
-				result.add("mv: cannot overwrite non-directory '"
-						+ destPath.get(0) + "' with directory '"
-						+ srcPath.get(0) + "'");
-			}
-		}
-		return result;
-	}
+            }
+            else // move folder to file - give error
+            {
+                result.add(ICommand.FAILURE);
+                result.add("mv: cannot overwrite non-directory '"
+                        + destPath.get(0) + "' with directory '"
+                        + srcPath.get(0) + "'");
+            }
+        }
+        return result;
+    }
 }
 
 // move file to folder will update path
